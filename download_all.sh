321b44077e019408cd32ead72dd3cbc0e2d26cdf
Your Name <you@example.com>
2020-11-10 07:00:49 +0000

    add run scripts

---
+++ download_all.sh
@@ -0,0 +1,6 @@
+python data/download_data.py --resource data.gold_passages_info.nq_train
+python data/download_data.py --resource data.gold_passages_info.nq_dev
+python data/download_data.py --resource data.gold_passages_info.nq_test
+python data/download_data.py --resource data.retriever_results.nq.single.train
+python data/download_data.py --resource data.retriever_results.nq.single.dev
+python data/download_data.py --resource data.retriever_results.nq.single.test
---
+++ run.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+python -m torch.distributed.launch --nproc_per_node 8 train_reader.py \
+--seed 42 \
+--learning_rate 1e-4  \
+--eval_step 1000 \
+--eval_top_docs 10  \
+--encoder_model_type t5 \
+--pretrained_model_cfg allenai/unifiedqa-t5-large-fid \
+--gold_passages_src data/data/gold_passages_info/nq_train.json \
+--gold_passages_src_dev data/data/gold_passages_info/nq_dev.json \
+--train_file data/data/retriever_results/nq/single/train.json \
+--dev_file data/data/retriever_results/nq/single/dev.json \
+--warmup_steps 0 \
+--sequence_length 250 \
+--batch_size 1 \
+--gradient_accumulation_steps 8 \
+--passages_per_question 10 \
+--num_train_epochs 100000 \
+--dev_batch_size 2 \
+--passages_per_question_predict 10 \
+--log_batch_step 20 \
+--output_dir myoutput_dir
